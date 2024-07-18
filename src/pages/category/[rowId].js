795f158fb3e364431fa1f958e419a098bdafc51e
Vivikth Erapalli <vivikth.erapalli@gmail.com>
2024-07-18 13:36:56 +0530

    Row id handle next -2

--- src/pages/category/[rowId].js
+++ src/pages/category/[rowId].js
@@ -1764,7 +1764,8 @@ const RowDetails = ({ row, totalPages ,rowIndex,rows}) => {
         
         // Show success alert
         alert('Change made successfully!');
-        {handleNext}
+        // router.back();
+        handleNext();
       } else {
         console.error('Failed to update row');
         // Remove the saving message
